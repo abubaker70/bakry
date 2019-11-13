a0db1de48b4ec23977f45177565394344b0f27c2
abubaker70 <57699491+abubaker70@users.noreply.github.com>
2019-11-13 09:20:56 +0300

    Add files via upload

---
+++ build/precache-manifest.9409320a0fba7f8a281a68b33dab4ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9238f30828f1af9aa3e75141d7fd31b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0498872065a6bbc259",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b4950b291b5b1ddb4c30",
+    "url": "/static/js/2.6ba25049.chunk.js"
+  },
+  {
+    "revision": "3b0498872065a6bbc259",
+    "url": "/static/js/main.ff1128f1.chunk.js"
+  },
+  {
+    "revision": "c9a1afe537f89f648632",
+    "url": "/static/js/runtime-main.14f7ac82.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "./App.css";
+import Sound from "react-sound";
+import Slider from "@material-ui/core/Slider";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+class App extends Component {
+  state = {
+    mute: false,
+    play: true,
+    display: 0,
+    value: 50
+  };
+
+  // handleMute = () => {
+  //   console.log(this.state.mute);
+  //   this.setState({ mute: !this.state.mute });
+  // };
+  // handlePlay = () => {
+  //   console.log(this.state.play);
+  //   this.setState({ play: !this.state.play });
+  // };
+  handlesound = x => {
+    this.setState({ value: x });
+    console.log(x);
+  };
+
+  handleChange = (event, newValue) => {
+    console.log(this.state.value);
+    this.setState({ value: newValue });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {/* <ReactAudioPlayer src="./beats/stolenbeat.mp3"  autoPlay /> */}
+        <Sound
+          url="./beats/stolenbeat.mp3"
+          volume={this.state.value}
+          playStatus={
+            this.state.display === 0
+              ? Sound.status.PLAYING
+              : this.state.display === 1
+              ? Sound.status.PAUSED
+              : Sound.status.STOPPED
+          }
+        />
+
+        <Typography id="continuous-slider" gutterBottom>
+          Volume
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider
+              value={this.state.value}
+              onChange={this.handleChange}
+              aria-labelledby="continuous-slider"
+            />
+          </Grid>
+          <Grid item>
+            <VolumeUp />
+          </Grid>
+        </Grid>
+        {/* <Slider
+          disabled
+          defaultValue={30}
+          aria-labelledby="continuous-slider"
+        />
+
+        <Slider
+          disabled
+          defaultValue={30}
+          aria-labelledby="continuous-slider"
+        /> */}
+        <button onClick={() => this.handlesound(0)}>play</button>
+        <button onClick={() => this.handlesound(1)}>pause</button>
+        <button onClick={() => this.handlesound(2)}>stop</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
